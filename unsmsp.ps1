b211ffb4bb6e18b23bd540a25ae6e6fb592065dd
jessetn <19999918+jessetn@users.noreply.github.com>
2019-11-27 14:34:28 -0600

    Update unsmsp.ps1

--- unsmsp.ps1
+++ unsmsp.ps1
@@ -1,3 +1,11 @@
-$wc.DownloadFile('https://www.dropbox.com/s/fet9dwb3p0nmuf2/UninstallView.exe?dl=1','C:\temp\UninstallView.exe')
+#Check OS
+if ([System.IntPtr]::Size -eq 4)
+{
+    $dropboxUrl = 'https://www.dropbox.com/s/z1or9kwndi0dz3t/UninstallView_x64.exe?dl=1'
+} else {
+    $dropboxUrl = 'https://www.dropbox.com/s/fet9dwb3p0nmuf2/UninstallView.exe?dl=1'
+}
+
+$wc.DownloadFile("$dropboxUrl",'C:\temp\UninstallView.exe')
 
 c:\temp\UninstallView.exe /quninstall "Syncro"
