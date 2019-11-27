b2425c83944435746ba491f4d30c866c63bb523f
jessetn <19999918+jessetn@users.noreply.github.com>
2019-11-27 15:59:27 -0600

    Update unsmsp.ps1

--- unsmsp.ps1
+++ unsmsp.ps1
@@ -1,11 +1,11 @@
 $wc = New-Object System.Net.WebClient
 
 #Check OS
-if ([System.IntPtr]::Size -eq 4)
-{
-    $dropboxUrl = 'https://www.dropbox.com/s/z1or9kwndi0dz3t/UninstallView_x64.exe?dl=1'
-} else {
+if ([System.IntPtr]::Size -eq 4) {
     $dropboxUrl = 'https://www.dropbox.com/s/fet9dwb3p0nmuf2/UninstallView.exe?dl=1'
+}
+else {
+    $dropboxUrl = 'https://www.dropbox.com/s/z1or9kwndi0dz3t/UninstallView_x64.exe?dl=1'
 }
 
 $wc.DownloadFile("$dropboxUrl",'C:\temp\UninstallView.exe')
